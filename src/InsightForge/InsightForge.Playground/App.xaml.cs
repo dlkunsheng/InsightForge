4148add8497292212e67216766a2a01f3f924771
崔驰坤 Cui, Richard Chikun <richardcuick@outlook.com>
2025-01-18 07:46:04 +0800

    create playground project for InsightForge

---
+++ src/InsightForge/InsightForge.Playground/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace InsightForge.Playground
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
