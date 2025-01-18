3faf4ad46b9405433f750f4f9f248d0c3d503ef6
崔驰坤 Cui, Richard Chikun <richardcuick@outlook.com>
2025-01-18 11:04:10 +0800

    create display info page

--- src/InsightForge/InsightForge.Playground/MainWindow.xaml.cs
+++ src/InsightForge/InsightForge.Playground/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using InsightForge.Playground.Pages;
 
 namespace InsightForge.Playground
 {
@@ -20,29 +21,18 @@ namespace InsightForge.Playground
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
         }
 
-        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private void DisplayInfoMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            await webView.EnsureCoreWebView2Async(null);
-            //webView.CoreWebView2.Navigate("https://www.example.com"); // 替换为您的测试页面 URL
-            //string htmlFilePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Web\\display-info.html");
-            //webView.CoreWebView2.Navigate($"file:///{htmlFilePath.Replace("\\", "/")}");
-            LoadHtmlFile(); 
-        }
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (webView.CoreWebView2 != null)
-            {
-                LoadHtmlFile();
-            }
+            // 使用 Frame 进行导航
+            mainFrame.Navigate(new DisplayInfo());
         }
 
-        private void LoadHtmlFile()
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            string htmlFilePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Web\\display-info.html");
-            webView.CoreWebView2.Navigate($"file:///{htmlFilePath.Replace("\\", "/")}");
+            // 退出应用
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ src/InsightForge/InsightForge.Playground/Pages/DisplayInfo.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InsightForge.Playground.Pages
+{
+    /// <summary>
+    /// DisplayInfo.xaml 的交互逻辑
+    /// </summary>
+    public partial class DisplayInfo : Page
+    {
+        public DisplayInfo()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            //webView.CoreWebView2.Navigate("https://www.example.com"); // 替换为您的测试页面 URL
+            //string htmlFilePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Web\\display-info.html");
+            //webView.CoreWebView2.Navigate($"file:///{htmlFilePath.Replace("\\", "/")}");
+            LoadHtmlFile();
+        }
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (webView.CoreWebView2 != null)
+            {
+                LoadHtmlFile();
+            }
+        }
+
+        private void LoadHtmlFile()
+        {
+            string htmlFilePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), $"Web\\display-info.html?nocache={DateTime.Now.ToString("yyyyMMddHHmmssfff")}");
+            webView.CoreWebView2.Navigate($"file:///{htmlFilePath.Replace("\\", "/")}");
+        }
+    }
+}
